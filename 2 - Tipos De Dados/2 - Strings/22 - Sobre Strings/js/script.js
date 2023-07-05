5d06f7b8e02ffee6840d54a107bdc1da99bd2e6c
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-07-04 23:03:19 -0300

    feat: data types

---
+++ 2 - Tipos De Dados/2 - Strings/22 - Sobre Strings/js/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nSegunda Linha");
+console.log("Meu nome é 'Wagner'");
+console.log('O nome dela é "Jenifer"');
+console.log(`A multiplicação de 2 * 2 é ${2 * 2}`);
+console.log("O meu nome é Wagner" + " e tenho 23 anos!");
---
+++ 2 - Tipos De Dados/3 - Booleans/25 - Operadores Lógicos/js/script.js
@@ -0,0 +1,6 @@
+console.log(5 > 3 && 3 === 2); // True && False
+console.log(5 > 3 || 3 == 1); // True || False
+console.log(3 == 3 && "Wagner" == "Wagner"); // True && True
+console.log("Felipe" == "João" || false); // False || False
+
+console.log(!(true && true)); // ! inverte o resultado de true
