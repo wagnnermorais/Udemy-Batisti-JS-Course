b717d240463e4301b70c8d13188945d2c70409c7
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-03 22:52:47 -0300

    feat: variables and built in functions

---
+++ 4 - Estrutura de Programacao/Built-In Functions/math.js
@@ -0,0 +1,5 @@
+let maior = Math.max(2, 5, 12, 28, 33);
+let menor = Math.min(2, 5, 12, 28, 33);
+let arredonda = Math.round(5.321321);
+let arredondaPraCima = Math.ceil(5.321312);
+console.log(maior, menor, arredonda, arredondaPraCima);
---
+++ 4 - Estrutura de Programacao/Variáveis/main.js
@@ -0,0 +1,7 @@
+var nome = "Matheus";
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+// ip = "312321"; -> Ocorre o erro "Assignment to constant variable". Pois o valor de uma constante não pode ser alterado.
