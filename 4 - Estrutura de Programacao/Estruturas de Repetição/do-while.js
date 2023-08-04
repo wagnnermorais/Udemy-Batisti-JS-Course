2acae844a7202ee616ecc6765c07c56d24e4bc6b
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-03 23:45:10 -0300

    feat: js loops

---
+++ 4 - Estrutura de Programacao/Estruturas de Repetição/do-while.js
@@ -0,0 +1,6 @@
+let y = 0;
+
+do {
+  y++;
+  console.log(y);
+} while (y < 5);
---
+++ 4 - Estrutura de Programacao/Estruturas de Repetição/switch-case.js
@@ -0,0 +1,13 @@
+let nome = "Wagner";
+
+switch (nome) {
+  case "Matheus":
+    console.log("O nome é Matheus");
+    break;
+  case "João":
+    console.log("O nome é João");
+    break;
+  case "Wagner":
+    console.log("O nome é Wagner!");
+    break;
+}
