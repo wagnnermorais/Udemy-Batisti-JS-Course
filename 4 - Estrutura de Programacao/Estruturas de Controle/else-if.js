0b66e4c90b288bc3e569229a9c0f9d328405dd86
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-03 23:16:31 -0300

    feat: conditionals

---
+++ 4 - Estrutura de Programacao/Estruturas de Controle/else-if.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 3;
+
+if (a + b == 3) {
+  console.log("O resultado é 3");
+} else if (a == 4) {
+  console.log("O valor de a é 4");
+} else if (b == 3) {
+  console.log("O valor de b é 3");
+} else {
+  console.log("Nenhuma das condições acima!");
+}
---
+++ 4 - Estrutura de Programacao/Estruturas de Controle/if.js
@@ -0,0 +1,15 @@
+let idade = 19;
+
+if (idade == 19) {
+  console.log("A idade é igual a 19!");
+}
+
+if (idade > 25) {
+  console.log("A idade é maior que 25)");
+}
+
+let nome = "Wagner";
+
+if (nome == "Wagner" && idade > 10) {
+  console.log("Liberado");
+}
