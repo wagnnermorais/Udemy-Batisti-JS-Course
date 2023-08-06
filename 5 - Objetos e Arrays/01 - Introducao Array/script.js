92f6722ca4a7a7396e6a49c50d53164cf22d2111
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-05 21:26:50 -0300

    feat: introducing arrays and objects

---
+++ 5 - Objetos e Arrays/01 - Introducao Array/script.js
@@ -0,0 +1,14 @@
+let array = [1, 4, 5, 6, 7];
+
+let nomes = ["Wagner", "Matheus", "Mãe"];
+
+let bool = [true, false, true, true];
+
+let arrayMisturado = [1, "Wagner", true];
+
+// acessando elementos de um array
+
+console.log(array[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arrayMisturado[2]);
