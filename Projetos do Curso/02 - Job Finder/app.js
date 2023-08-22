6cd1b0b1675054d97206d57484933faa710d692a
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-22 17:41:45 -0300

    using body parser

--- Projetos do Curso/02 - Job Finder/app.js
+++ Projetos do Curso/02 - Job Finder/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const db = require("./db/connection");
 const PORT = 3000;
+const bodyParser = require("body-parser");
 
 app.listen(PORT, () => {
   console.log(`Express rodando na porta ${PORT}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 try {
@@ -13,10 +16,13 @@ try {
     console.log("Conectou ao banco com sucesso.");
   });
 } catch (error) {
-  console.log("Ocorreu um erro ao conectar ao banco");
+  console.log("Ocorreu um erro ao conectar ao banco.");
 }
 
 // routes
 app.get("/", (request, response) => {
   response.send("Está funcionando.");
 });
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
