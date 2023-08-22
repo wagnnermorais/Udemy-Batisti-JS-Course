2d1e80bef315f17d06176366f5af351bac4e9573
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-22 17:40:29 -0300

    creating routes index

---
+++ Projetos do Curso/02 - Job Finder/routes/jobs.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../models/Job");
+
+router.get("/test", (request, response) => {
+  response.send("Deu certo");
+});
+
+// add job via post
+router.post("/add", (request, response) => {
+  let { title, salary, company, description, email, new_job } = request.body;
+
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job,
+  })
+    .then(() => response.redirect("/"))
+    .catch((error) => console.log(error));
+});
+
+module.exports = router;
