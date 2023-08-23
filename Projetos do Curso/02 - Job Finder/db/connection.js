ed1a4587e4d2b27cc79889346511d3277571e2cd
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-23 17:48:41 -0300

    refac: changing db path

--- Projetos do Curso/02 - Job Finder/db/connection.js
+++ Projetos do Curso/02 - Job Finder/db/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: ".db/app.db",
+  storage: "db/app.db",
 });
 
 module.exports = sequelize;
